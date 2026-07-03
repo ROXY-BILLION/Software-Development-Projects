4b5e781034475858f60c8a11f521606b11b9076e
giftdivine <giftdivine787@gmail.com>
2026-07-03 16:14:05 +0100

    initial

---
+++ Simple-Calculator/script.js
@@ -0,0 +1,7 @@
+let num1 = Number(prompt("Enter first number"));
+let num2 = Number(prompt("Enter Second number"));
+
+console.log(`Addition: ${num1 + num2}`);
+console.log(`Subtraction: ${num1 - num2}`);
+console.log(`Multiplication: ${num1 * num2}`);
+console.log(`Division: ${num1 / num2}`);
