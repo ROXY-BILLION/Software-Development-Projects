80bcfb953f7534b0b709f03e4d6fc96c3d38f816
giftdivine <giftdivine787@gmail.com>
2026-07-06 14:50:47 +0100

    Student Information System

---
+++ PHASE 1/Student Information System/script.js
@@ -0,0 +1,14 @@
+let student = {
+    name: "Divine",
+    age: 21,
+    department: "Computer Science",
+    email: "giftdivine787@gmail.com"
+}
+let student2 = {
+    name: "Miracle",
+    age: 28,
+    department: "Bio Chemistry",
+    email: "giftb123@gmail.com"
+}
+console.log(Object.keys(student));
+console.log(Object.entries(student2));
