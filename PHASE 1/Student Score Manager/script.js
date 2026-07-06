8b9a5a01ce1f49d05186fc410e8e98c4cd7f8c76
giftdivine <giftdivine787@gmail.com>
2026-07-06 13:59:30 +0100

    Student Score  Manager

---
+++ PHASE 1/Student Score Manager/script.js
@@ -0,0 +1,12 @@
+let score = [56, 89, 87, 90];
+score.push("78")
+console.log(score);
+
+score.pop();
+console.log(score);
+
+score.unshift(877);
+console.log(score);
+
+score.shift(877);
+console.log(score);
